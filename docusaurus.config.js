037ada7cd92f08ea098dc00f4c594a9d52a12fc7
Samuel Mediani <66485729+SamMed05@users.noreply.github.com>
2022-07-26 15:52:25 +0200

    modify announcementBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
   	  announcementBar: {
     		id: 'support_us',
     		content:
-    		  'This website is under construction. 🚧 Please be patient to see more content.',
+    		  'This website is under construction. 🚧 Please be patient more content.',
     		backgroundColor: '#fafbfc',
     		textColor: '#091E42',
     		isCloseable: true,
